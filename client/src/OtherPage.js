c78a2eb528073f85ee73feb24ad7cf65522f4cc9
agorski <adriangorski996@gmail.com>
2020-07-19 00:17:55 +0200

    initial commit

---
+++ client/src/OtherPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+
+export default () => {
+    return (
+        <div>
+            In some other page
+            <Link to="/">
+                Go home
+            </Link>
+        </div>
+    );
+};
